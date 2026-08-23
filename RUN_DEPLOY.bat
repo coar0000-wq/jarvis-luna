943dfe2f02cde5e3e81ba1e12153ab90aad99a90
JARVIS LUNA <coar0000@naver.com>
2026-08-23 15:19:23 +0900

    JARVIS: restyle Daiso card as Shopify dropshipping panel

--- RUN_DEPLOY.bat
+++ RUN_DEPLOY.bat
@@ -1,6 +1,7 @@
 @echo off
 set REPO=C:\Users\Desktop\Claude\Projects\kms\jarvis-luna
+set JARVIS_MSG=JARVIS: restyle Daiso card as Shopify dropshipping panel
 cd /d "%REPO%"
-echo === BACKUP %DATE% %TIME% === >"%REPO%\deploy_stdout.txt"
-python "%REPO%\scripts\backup_repo.py" >>"%REPO%\deploy_stdout.txt" 2>&1
+echo === START %DATE% %TIME% === >"%REPO%\deploy_stdout.txt"
+python "%REPO%\JARVIS_AUTO_DEPLOY.py" >>"%REPO%\deploy_stdout.txt" 2>&1
 echo === END %TIME% === >>"%REPO%\deploy_stdout.txt"
